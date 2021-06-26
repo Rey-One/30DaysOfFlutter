2a9ebb1d86d5f6f59c8968c3ce891df2656d7fc2
Rey-One <rohitmaurya780@gemail.com>
2021-06-27 00:25:48 +0530

    day-3  complete

--- simpleproject/lib/Screen_Page/home_page.dart
+++ simpleproject/lib/Screen_Page/home_page.dart
@@ -17,7 +17,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(
-        title:Text("Shvetagit app"),
+        title:Text("Demo app"),
 
 
       ),
--- simpleproject/lib/main.dart
+++ simpleproject/lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'Screen_Page/home_page.dart';
+import 'Screen_Page/login_page.dart';
 
-void main()
-{
+void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-  
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-
-   
-
-    return MaterialApp( 
-        home:HomePage(),
-      );
+    return MaterialApp(
+  //    home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home":(context)=> HomePage(),
+        "/login":(context)=> LoginPage(),
+      },
+    );
   }
 }
-
