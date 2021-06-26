ab9bbdf5f971deaed5f70a0fedcbaae9e069d62f
Rey-One <rohitmaurya780@gemail.com>
2021-06-26 22:27:17 +0530

    first day

---
+++ simpleproject/lib/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  final int days = 30;
+  final String Str = "Hello Rohit1";
+
+  //  double  pi =3.14;
+  // bool ismale= true;
+  // num temp=30.5;   // its take both type of data type(int , double)
+
+  //  var  day="Monday"; // its takes all type of datatype
+
+  //  const pi=3.14;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title:Text("Shvetagit app"),
+
+
+      ),
+
+      body: Center(
+        child: Container(
+          child: Text("Welcom to $days My New App $Str"),
+        ),
+      ),
+      drawer:Drawer(),
+    );
+  }
+}
